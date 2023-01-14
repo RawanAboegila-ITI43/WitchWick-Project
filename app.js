6b40abdd9a513a1b875e1641fe756774562ffa51
RawanAboegila-ITI43 <rawan.moyasser.aboegila@gmail.com>
2023-01-15 00:22:21 +0200

    navigation bar bugs fixed (but one) + added navbar scroll animation + added rest of webpages and linked them

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let slideIndex = 0;
 showSlides();
 
+
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
@@ -17,10 +18,29 @@ function showSlides() {
 function menuBar() {
     var x = document.getElementById("links");
     if (x.style.display === "block") {
-        console.log("none");
         x.style.display = "none";
     } else {
-        console.log("block");
         x.style.display = "block";
     }
 }
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('.container');
+    var offset = startchange.offset();
+    console.log("scroll");
+    console.log(startchange);
+    console.log(offset);
+    $(document).scroll(function () {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $('.container').css('background-color', '#fefefe');
+            $('.container').css('box-shadow', '0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)');
+        } else {
+            $('.container').css('background', 'transparent');
+            $('.container').css('border', 'none');
+            $('.container').css('box-shadow', 'none');
+
+        }
+    });
+})
