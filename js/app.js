dc867ae2fe07b6cf77e2066127ab29694146d5cb
RawanAboegila-ITI43 <rawan.moyasser.aboegila@gmail.com>
2023-01-15 23:31:01 +0200

    reorganized files + added login and register form (+validation) + fixed web pages linking bugs

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
-let slideIndex = 0;
-showSlides();
 
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-    return slides;
-}
 
 function menuBar() {
     var x = document.getElementById("links");
@@ -25,6 +10,16 @@ function menuBar() {
 }
 
 $(document).ready(function () {
+    var loginTrans = {
+
+        color: ' #755441',
+        backgroundColor: ' #fefefe'
+    }
+    var loginNorm = {
+
+        color: '  #fefefe',
+        backgroundColor: ' #755441'
+    };
     var scroll_start = 0;
     var startchange = $('.container');
     var offset = startchange.offset();
@@ -36,11 +31,14 @@ $(document).ready(function () {
         if (scroll_start > offset.top) {
             $('.container').css('background-color', '#fefefe');
             $('.container').css('box-shadow', '0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12)');
+            $('#loginLink').css(loginNorm);
         } else {
             $('.container').css('background', 'transparent');
             $('.container').css('border', 'none');
             $('.container').css('box-shadow', 'none');
-
+            $('#loginLink').css(loginTrans);
         }
     });
+
+
 })
